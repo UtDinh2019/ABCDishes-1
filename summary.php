b94bea5d4304c6938c90d404a0e6b83fd8b5273a
Dylon Lowinske <dlowinske@users.noreply.github.com>
2020-03-03 20:25:08 -0600

    Added Pie chart summary

--- summary.php
+++ summary.php
@@ -1,49 +1,62 @@
 <?php
-  $nav_selected = "SCANNER";
+	
+  // set the current page; 
+  $nav_selected = "SUMMARY";
+
+  // make the left menu buttons visible; options: YES, NO
   $left_buttons = "NO";
-  $left_selected = "RELEASES";
+
+  // set the left menu button selected; options will change based on the main selection
+  $left_selected = "";
 
   include("./nav.php");
+?>
 
-  require 'db_configuration.php';
-
-  $queryInReview = "SELECT Names FROM dishes where status = 'In Review'";
-  $queryProposed = "SELECT Names FROM dishes where status = 'Proposed'";
-  $queryApproved = "SELECT Names FROM dishes where status = 'Approved'";
-
-
-  $GLOBALS['dataInReview'] = mysqli_query($db, $queryInReview);
-  $GLOBALS['dataProposed'] = mysqli_query($db, $queryProposed);
-  $GLOBALS['dataApproved'] = mysqli_query($db, $queryApproved);
-
-
-
-  
- ?>
-
- <div class="right-content">
-    <div class="container">
-
-      <h3 style = "color: #01B0F1;">Summary</h3>
-      
-
-      <table>
- <tr>
-  <th>Name</th>
-  <th>Favorite Color</th>
- </tr>
- <tr>
-  <td>Bob</td>
-  <td>Yellow</td>
- </tr>
- <tr>
-  <td>Michelle</td>
-  <td>Purple</td>
- </tr>
-</table>
-
-
-    </div>
-</div>
 
 <?php include("./footer.php"); ?>
+
+
+
+ <?php  
+ $connect = mysqli_connect("localhost", "root", "", "abcd_db");  
+ $query = "SELECT Status, count(*) as number FROM dishes GROUP BY Status";  
+ $result = mysqli_query($connect, $query);  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Summary of the Status</title>  
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+           <script type="text/javascript">  
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['Status', 'Number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["Status"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Percentage of approved  ',  
+                      //is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div style="width:900px;">  
+                <h3 align="center">Summary of the Status</h3>  
+                <br />  
+                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+           </div>  
+      </body>  
+ </html>  
