e8bec19c344d9771b4c529a69f08d787a1e5ae46
Dylon Lowinske <dylon.lowinske@gmail.com>
2020-04-03 18:27:10 -0500

    Added compile to nav bar

---
+++ compile.php
@@ -0,0 +1,13 @@
+<?php
+	
+  $nav_selected = "COMPILE";
+  $left_buttons = "NO";
+  $left_selected = "";
+
+  include("./nav.php");
+?>
+
+
+<?php include("./footer.php"); ?>
+<link rel="stylesheet" href="css/mainStyleSheet.css" type="text/css">
+
